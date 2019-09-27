630f8016b0c82e6a6d30b92591a214d294266e2d
Amim Knabben <amim.knabben@gmail.com>
2019-09-27 19:01:38 -0400

    Api CRD

---
+++ pkg/apis/chat/v1alpha1/chat_types.go
@@ -0,0 +1,50 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// ChatSpec defines the desired state of Chat
+// +k8s:openapi-gen=true
+type ChatSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// ChatStatus defines the observed state of Chat
+// +k8s:openapi-gen=true
+type ChatStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// Chat is the Schema for the chats API
+// +k8s:openapi-gen=true
+// +kubebuilder:subresource:status
+type Chat struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   ChatSpec   `json:"spec,omitempty"`
+	Status ChatStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// ChatList contains a list of Chat
+type ChatList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Chat `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Chat{}, &ChatList{})
+}

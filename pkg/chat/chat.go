12393f2e9c5ef184f3d34a617818391290fa8f4a
Amim Knabben <amim.knabben@gmail.com>
2019-10-09 17:05:24 -0400

    Fixing chat structure

--- pkg/chat/chat.go
+++ pkg/chat/chat.go
@@ -3,73 +3,101 @@ package chat
 import (
 	"context"
 	"fmt"
+	"github.com/go-logr/logr"
 	chatv1 "github.com/knabben/chatops/api/v1"
 	"github.com/nlopes/slack"
-	"os"
-	c "sigs.k8s.io/controller-runtime/pkg/client"
+	cl "sigs.k8s.io/controller-runtime/pkg/client"
+	"strings"
 	"time"
 )
 
-var (
-	token = os.Getenv("TOKEN")
-)
+type Chat struct {
+	cl.Client
+	SlackClient *slack.Client
+	Log         logr.Logger
+}
 
-func UpdateItems(chatList *chatv1.ChatList, chatStatus *chatv1.Chat, client c.Client) {
+func NewChat(token string, client cl.Client) *Chat {
+	return &Chat{SlackClient: slack.New(token), Client: client}
+}
+
+func (c *Chat) SendMessage(message string) {
+	if _, _, err := c.SlackClient.PostMessage("CKY72UCH1", slack.MsgOptionText(message, false)); err != nil {
+		c.Log.Error(err, "Error trying to send a message via slack.")
+	}
+}
+
+func (c *Chat) UpdateItem(chat *chatv1.Chat, status chatv1.ChatStatus) {
+	item := chat.DeepCopy()
+	item.Spec.Timestamp = time.Now().Unix()
+	item.Status = status
+
+	if err := c.Client.Update(context.Background(), item); err != nil {
+		fmt.Println("Update ERROR", err)
+		return
+	}
+}
+
+func (c *Chat) commandInCRD(command string) (*chatv1.Chat, error) {
+	var chatList = &chatv1.ChatList{}
+	if err := c.List(context.Background(), chatList, &cl.ListOptions{}); err != nil {
+		fmt.Println("List ERROR", err)
+		return nil, err
+	}
+
 	for _, item := range chatList.Items {
-		chatItem := item.DeepCopy()
-		chatItem.Spec.Timestamp = time.Now().Unix()
-		if err := client.Update(context.Background(), chatItem); err != nil {
-			fmt.Println("UpdateERROR", err)
-			continue
+		if item.Spec.Command == command {
+			return &item, nil
 		}
 	}
+
+	return nil, nil
 }
 
-func ChangeCRD(inputChannel chan *chatv1.Chat, outputChannel chan string, client c.Client) {
-	api := slack.New(token)
-
-	for {
-		chat := <-inputChannel
-		switch chat.Status.Command {
-		case "chat":
-			var chatList = &chatv1.ChatList{}
-			if err := client.List(context.Background(), chatList, &c.ListOptions{}); err != nil {
-				fmt.Println("List error", err)
-				continue
-			}
-
-			UpdateItems(chatList, chat, client)
-
-			if _, _, err := api.PostMessage("CKY72UCH1", slack.MsgOptionText("ok", false)); err != nil {
-				fmt.Println(err)
-			}
-			fmt.Println("-------")
-		}
-	}
-}
-
-func ListenChat(inputChannel chan *chatv1.Chat) {
-	api := slack.New(token)
-
-	rtm := api.NewRTM()
+// ListenChat listen for events in the chat channel
+func (c *Chat) ListenChat(inputChannel chan *chatv1.ChatStatus) {
+	rtm := c.SlackClient.NewRTM()
 	go rtm.ManageConnection()
 
 	for msg := range rtm.IncomingEvents {
 		switch ev := msg.Data.(type) {
 
 		case *slack.MessageEvent:
-			if ev.User != "" {
-				fmt.Printf("Message: %v, %s\n", ev.Text, ev.User)
-				inputChannel <- &chatv1.Chat{
-					Status: chatv1.ChatStatus{
-						Command:  ev.Text,
-						Username: ev.User,
-					},
-				}
+			chatStatus := c.ExtractChatStatus(ev)
+			if ev.User != "" || chatStatus != nil { // filter commands here before listing on CRD?
+				inputChannel <- chatStatus
 			}
 
 		case *slack.RTMError:
 			fmt.Printf("Error: %s\n", ev.Error())
 		}
+	}
+}
+
+// ChangeCRD updates the resource definition based on Slack events
+func (c *Chat) ChangeCRD(inputChannel chan *chatv1.ChatStatus) {
+	for {
+		chatStatus := <-inputChannel
+		if chat, err := c.commandInCRD(chatStatus.Command); err == nil {
+			c.UpdateItem(chat, *chatStatus)
+		}
+	}
+}
+
+// ExtractChatStatus returns a translated content of chat status
+func (c *Chat) ExtractChatStatus(message *slack.MessageEvent) *chatv1.ChatStatus {
+	fmt.Println(fmt.Sprintf("Message: %v, %s\n", message.Text, message.User))
+
+	tokens := strings.Split(message.Text, " ")
+	if len(tokens) == 1 {
+		return nil
+	}
+
+	return &chatv1.ChatStatus{
+		Command:   tokens[0],
+		Arguments: strings.Join(tokens[1:], " "),
+		Username:  message.Username,
+		Timestamp: time.Now().String(),
+		Channel:   message.Channel,
 	}
 }
---
+++ pkg/chat/chat_test.go
@@ -0,0 +1,14 @@
+package chat
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestNewChat(t *testing.T) {
+	assert := assert.New(t)
+	//fake_token := "token"
+
+	//chat := NewChat(fake_token)
+	assert.Equal(nil, "element")
+}
